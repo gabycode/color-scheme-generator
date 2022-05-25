07a99e9410250d80a392b8a61dcf18926b5a3748
gabycode <86427446+gabycode@users.noreply.github.com>
2022-05-25 10:50:26 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+const inputColor = document.getElementById('color')
+const selectOptions = document.getElementById('options')
+const colorBtn = document.getElementById('btn')
+const schemeDisplay = document.getElementById('scheme-display')
