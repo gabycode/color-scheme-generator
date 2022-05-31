a9fdd26f0dac2e349c0ef2d8219bc8e048771a87
gabycode <86427446+gabycode@users.noreply.github.com>
2022-05-31 10:01:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,35 @@ const inputColor = document.getElementById('color')
 const selectOptions = document.getElementById('options')
 const colorBtn = document.getElementById('btn')
 const schemeDisplay = document.getElementById('scheme-display')
+
+colorBtn.addEventListener('click', e => {
+    e.preventDefault() 
+
+    // Store hex values in variables
+    let currentColor = inputColor.value.substring(1)
+    let currentColorScheme = selectOptions.value
+    
+    let colorSchArray = []
+    let colorSchHtml = ""
+
+    fetch(`https://www.thecolorapi.com/scheme?hex=${currentColor}&mode=${currentColorScheme}`)
+        .then(resp => resp.json())
+        .then(data => {
+
+            // Loops through colors array
+            for(let i = 0; i < data.colors.length; i++){
+                    colorSchArray.push(data.colors[i].hex.value)
+                }
+                console.log(colorSchArray)
+            
+            // Displays color scheme
+            for(let color of colorSchArray){
+                colorSchHtml += `
+                <div class="color-scheme">
+                    <div class="color-item" style="background-color: ${color}"><p>${color}</p></div>
+                </div>
+                `
+            }
+            schemeDisplay.innerHTML = colorSchHtml
+        })
+})
